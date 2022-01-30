0396d0c6ac536afb6298a5964158455ef0088e65
xavioo <rucahoxavi@gmail.com>
2022-01-30 15:31:02 +0200

    update using patch to update particular property in body

--- backend/routes/todos.js
+++ backend/routes/todos.js
@@ -69,6 +69,23 @@ router.put("/:id",async (req, res) => {
 });
 
 
+router.patch("/:id", async (req, res) => {
+  const todo = await Todo.findById(req.params.id);
+
+  if (!todo) return res.status(404).send("Todo not found...");
+  const updatedTodo = await Todo.findByIdAndUpdate(
+    req.params.id,
+    {
+      isComplete: !todo.isComplete,
+    },
+    {
+      new: true,
+    }
+  );
+  res.send(updatedTodo);
+});
+
+
 
 
 module.exports = router;
