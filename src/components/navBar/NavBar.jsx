4a96bbf3e7c4049bb6d666183012057988a075d3
xavioo <rucahoxavi@gmail.com>
2022-02-07 06:56:58 +0200

    make styles on navbar

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,31 +1,52 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
 
 import { AppBar, Typography, Toolbar, Button } from "@material-ui/core";
 import { Link } from "react-router-dom";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  authButton: {
+    right: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  linkStyle: {
+    textDecoration: "none",
+    color: "#fafafa",
+  },
+}));
+
+
 const NavBar = () => {
+  const classes = useStyles();
+
   return (
     <>
-      <div className="">
+        <div className={classes.root}>
         <AppBar position="static" color="primary">
           <Toolbar>
-            <Typography variant="h4" className="">
-              <Link className="" to="/">
+            <Typography variant="h4"className={classes.title}>
+              {/* <Link className="" to="/"> */}
+              <Link className={classes.linkStyle} to="/">
                 toDoApp;
               </Link>
             </Typography>
            
               <>
-                <Typography variant="subtitle2" className="">
+                <Typography variant="subtitle2"className={classes.title}>
                   Logged in as xavio
                 </Typography>
                 <Button
                   edge="end"
                   color="inherit"
-                  className=""
+                  className={classes.authButton}
                   
                 >
-                  <Link className="" to="/">
+                  <Link className={classes.linkStyle} to="/">
                     SignOut
                   </Link>
                 </Button>
@@ -37,19 +58,21 @@ const NavBar = () => {
                   color="inherit"
                   className=""
                 >
-                  <Link className="" to="/signin">
+                 <Link className={classes.linkStyle} to="/signin">
                     SignIn
                   </Link>
                 </Button>
+
                 <Button
                   edge="end"
                   color="inherit"
                   className=""
-                >
-                  <Link className="" to="/signup">
+                  > 
+                  <Link className={classes.linkStyle} to="/signup">
                     SignUp
                   </Link>
                 </Button>
+
               </>
           
           </Toolbar>
