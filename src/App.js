2f19479660366116e8683be5a478355c5ab25b01
xavioo <rucahoxavi@gmail.com>
2022-02-07 13:11:03 +0200

    add margin to app.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
 import React from 'react';
 import './App.css';
+import { makeStyles } from "@material-ui/core/styles";
+
 import { Container } from '@material-ui/core';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
-// import Todos from './components/todos/Todos';
 import SignIn from './components/auth/SignIn';
 import SignUp from './components/auth/SignUp';
 import NavBar from './components/navBar/NavBar';
 import Todos from './components/todos/Todos';
 
+const useStyles = makeStyles({
+  contentStyle: {
+    margin: "30px auto",
+  }
+});
+
 function App() {
+
+  const classes = useStyles();
 
   return (
     <>
     <BrowserRouter>
       <Container maxWidth = "md">
         <NavBar />
-        <Container className="" maxWidth = "sm">
+         <Container className={classes.contentStyle} maxWidth = "sm">
         <Routes> 
        <Route path='/signin' element={<SignIn />}/>
        <Route path='/signup' element={<SignUp />} />
