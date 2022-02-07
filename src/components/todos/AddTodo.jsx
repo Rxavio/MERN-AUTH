b1970c3d127ebef7086b99f64be366f0ba9a325e
xavioo <rucahoxavi@gmail.com>
2022-02-07 12:47:52 +0200

    styles list todos

--- src/components/todos/AddTodo.jsx
+++ src/components/todos/AddTodo.jsx
@@ -2,26 +2,38 @@ import React from 'react';
 
 import { TextField, Button } from '@material-ui/core';
 import { Send } from '@material-ui/icons';
+import { makeStyles } from "@material-ui/core/styles";
 
+const useStyles = makeStyles({
+    formStyle: {
+      margin: "0px auto",
+      padding: "30px",
+      borderRadius: "9px",
+      boxShadow: "0px 0px 12px -3px #000000",
+      display: "flex",
+      justifyContent: "space-between",
+    },
+    submitButton: {
+        marginLeft: "20px",
+    }
+  });
 
-
-
-const AddTodo = ({ todo, setTodo }) => {
-    
+const AddTodo = () => {
+    const classes = useStyles();
 
     return ( 
         <>
-            <form noValidate autoComplete="off" className="" onSubmit = "">
+            <form noValidate autoComplete="off" className={classes.formStyle} onSubmit = "">
                 <TextField
                     id="enter-todo"
-                    label="enterToDo"
+                    label="EnterToDo"
                     variant="outlined"
                     autoFocus
                     fullWidth
                     value = ""
                    
                 />
-                <Button variant="contained" color="primary" className = "" type="submit">
+                <Button variant="contained" color="primary" className = {classes.submitButton} type="submit">
                     <Send/>
                 </Button>
             </form>
--- src/components/todos/Todos.jsx
+++ src/components/todos/Todos.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import AddTodo from "./AddTodo";
+import ListTodos from "./ListTodos";
 
 const Todos = () => {
-
 
   return (
     <>
    <h3>Todos</h3>  
+   <AddTodo />
+   <ListTodos />
    </>
   );
 };
