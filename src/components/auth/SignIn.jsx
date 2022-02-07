6d54b3c49a55b22c91736e0b592f213e347774bd
xavioo <rucahoxavi@gmail.com>
2022-02-07 12:53:51 +0200

    style signin

--- src/components/auth/SignIn.jsx
+++ src/components/auth/SignIn.jsx
@@ -1,24 +1,38 @@
 import React from "react";
 import { Typography, TextField, Button } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
+const useStyles = makeStyles({
+  formStyle: {
+    margin: "0px auto",
+    padding: "30px",
+    borderRadius: "9px",
+    boxShadow: "0px 0px 12px -3px #000000",
+  },
+  spacing: {
+    marginTop: "20px",
+  },
+});
 const SignIn = () => {
+  const classes = useStyles();
   
   return (
     <>
       <form
         noValidate
+        className={classes.formStyle}
         autoComplete="off">
 
         <Typography variant="h5">signIn</Typography>
         <TextField
-          className=""
+           className={classes.spacing}
           id="enter-email"
           label="enterEmail"
           variant="outlined"
           fullWidth
         />
         <TextField
-          className=""
+          className={classes.spacing}
           id="enter-password"
           type="password"
           label="enterPassword"
@@ -28,7 +42,7 @@ const SignIn = () => {
         <Button
           variant="contained"
           color="primary"
-          className=""
+          className={classes.spacing}
           type="submit"
         >
           SignIn
